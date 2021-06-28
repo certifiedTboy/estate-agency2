a9d67bddf21a36d03a9e4025c5162778f4302147
certifiedTboy <etosin70@gmail.com>
2021-06-28 02:20:29 +0100

    edit app.js

--- app.js
+++ app.js
@@ -19,10 +19,10 @@ var Message = require("./models/message")
 var response = require("./models/response")
 var mongoose = require("mongoose"); 
 const User = require('./models/user');
-var dbUrl = 'mongodb://smssolution:yGHblWA4Vm4LFivj@cluster0-shard-00-00.3wo4h.mongodb.net:27017,cluster0-shard-00-01.3wo4h.mongodb.net:27017,cluster0-shard-00-02.3wo4h.mongodb.net:27017/estate-agency?authSource=admin&replicaSet=atlas-8os7kz-shard-0&w=majority&readPreference=primary&retryWrites=true&ssl=true'
 
-// var dbUrl = "mongodb://127.0.0.1:27017/estate-agency"
-mongoose.connect(dbUrl, {useNewUrlParser:true}, {useUnifiedTopology: true}, {useMongoClient : true})
+
+
+mongoose.connect('mongodb://smssolution:yGHblWA4Vm4LFivj@cluster0-shard-00-00.3wo4h.mongodb.net:27017,cluster0-shard-00-01.3wo4h.mongodb.net:27017,cluster0-shard-00-02.3wo4h.mongodb.net:27017/estate-agency?authSource=admin&replicaSet=atlas-8os7kz-shard-0&w=majority&readPreference=primary&retryWrites=true&ssl=true', {useNewUrlParser:true}, {useUnifiedTopology: true})
 .then(()=>console.log('connectd to db'))
 .catch((err)=>console.log('error ',err));
 
