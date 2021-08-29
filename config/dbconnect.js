7c6f15c629b5421f3d974201a8b726d1cb80ae66
certifiedTboy <79862233+certifiedTboy@users.noreply.github.com>
2021-08-29 18:34:37 +0100

    Update dbconnect.js

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 // var dbUrl = "mongodb://127.0.0.1:27017/estate-agency"
 const dbUrl = 'mongodb://:yGHblWA4Vm4LFivj@cluster0-shard-00-00.3wo4h.mongodb.net:27017,cluster0-shard-00-01.3wo4h.mongodb.net:27017,cluster0-shard-00-02.3wo4h.mongodb.net:27017/?authSource=admin&replicaSet=atlas-8os7kz-shard-0&w=majority&readPreference=primary&retryWrites=true&ssl=true'
-const connect = mongoose.connect(dbUrl, {useNewUrlParser:true, useUnifiedTopology: true})
+const dbConnect = mongoose.connect(dbUrl, {useNewUrlParser:true, useUnifiedTopology: true})
 .then(()=>console.log('connectd to db'))
 .catch((err)=>console.log('error ',err));
 
